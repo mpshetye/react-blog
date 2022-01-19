3d955305ec5daeaeaf2d90a7efeab731a7db1821
Manali Shetye <manalis826@gmail.com>
2022-01-20 01:04:47 +0530

    Modified file
    created a custom hook instead of previous code for reusability purpose and added itto the file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,12 @@
-import { useState, useEffect } from "react";
+import useFetch from "../customHooks/useFetch";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [loading, setLoading] = useState(true);
-  const [blogs, setBlogs] = useState(null);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        //error coming back from server due to resource not existing
-        if (!res.ok) {
-          throw Error("could not fetch the data");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setLoading(false);
-        setBlogs(data);
-        setError(null);
-      })
-      .catch((err) => {
-        // auto catches network/connection error
-        setLoading(false);
-        setError(err.message);
-      });
-  }, []);
+  const {
+    data: blogs,
+    loading,
+    error,
+  } = useFetch("http://localhost:8000/blogs"); //custom hook
   return (
     <div className="home">
       {error && <div>{error}</div>}
