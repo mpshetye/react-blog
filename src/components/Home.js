8fdaa4fef90d437b73f06c222dc0041b020f5172
Manali Shetye <manalis826@gmail.com>
2022-01-20 00:12:21 +0530

    Updated file
    added error handling

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,23 +2,37 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [blogs, setBlogs] = useState(null);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs').then(res => res.json).then(data =>{
-            setLoading(false);
-            setBlogs(data);
-        }).catch(err => console.log(err));
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        //error coming back from server due to resource not existing
+        if (!res.ok) {
+          throw Error("could not fetch the data");
         }
-    , []);
-    return (
-        <div className="home">
-            {loading && <div>Loading...</div>}
-            {blogs && <BlogList blogs={blogs}/>}
-            
-        </div>
-    );
-}
- 
+        return res.json();
+      })
+      .then((data) => {
+        setLoading(false);
+        setBlogs(data);
+        setError(null);
+      })
+      .catch((err) => {
+        // auto catches network/connection error
+        setLoading(false);
+        setError(err.message);
+      });
+  }, []);
+  return (
+    <div className="home">
+      {error && <div>{error}</div>}
+      {loading && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} />}
+    </div>
+  );
+};
+
 export default Home;
