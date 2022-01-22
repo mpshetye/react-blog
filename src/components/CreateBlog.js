aba664dcfad174564112f50b8d8b4150d0eadc43
Manali Shetye <manalis826@gmail.com>
2022-01-22 13:22:35 +0530

    Initial commit
    used form to create new blog

---
+++ src/components/CreateBlog.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+export default function CreateBlog() {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+    return <>
+    <div className="create">
+        <h2>Add New Blog</h2>
+        <form>
+            <label htmlFor="title">Title:</label>
+            <input type="text" id="title" value={title} required onChange={(e) =>{setTitle(e.target.value)}}/>
+            <label htmlFor="body">Body:</label>
+            <textarea id="body" cols="30" rows="10" value={body} required onChange={(e) => setBody(e.target.value)}></textarea>
+            <label htmlFor="author">Author:</label>
+            <input type="text" id="author" value ={author} onChange={(e) => setAuthor(e.target.value)}/>
+            <button>Add Blog</button>
+
+        </form>
+    </div>
+
+    </>;
+}
