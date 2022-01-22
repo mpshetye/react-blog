999b07aad55e461649b7958a8a6366e604fab5ac
Manali Shetye <manalis826@gmail.com>
2022-01-22 20:34:10 +0530

    Modified file
    fetched data from json server and handled error

--- src/components/CreateBlog.js
+++ src/components/CreateBlog.js
@@ -1,28 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function CreateBlog() {
-    const [title, setTitle] = useState('');
-    const [body, setBody] = useState('');
-    const [author, setAuthor] = useState('');
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        const blogDetails = {title, body, author};
-        console.log(blogDetails);
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+    const blogDetails = { title, body, author };
+    try {
+      const res = await fetch("http://localhost:8000/blogs", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(blogDetails),
+      });
+      console.log(res);
+      if (!res.ok) {
+        throw Error(`an error has occurred: ${res.status} ${res.statusText}`);
+      } else {
+        setLoading(false);
+      }
+    } catch (err) {
+      console.log(err);
+      setError(err.message);
     }
-    return <>
-    <div className="create">
-        <h2>Add New Blog</h2>
-        <form onSubmit={handleSubmit}>
+  };
+  return (
+    <>
+      {error ? (
+        <div> {error} </div>
+      ) : (
+        <div className="create">
+          <h2>Add New Blog</h2>
+          <form onSubmit={handleSubmit}>
             <label htmlFor="title">Title:</label>
-            <input type="text" id="title" value={title} required onChange={(e) =>{setTitle(e.target.value)}}/>
+            <input
+              type="text"
+              id="title"
+              value={title}
+              required
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            />
             <label htmlFor="body">Body:</label>
-            <textarea id="body" cols="30" rows="10" value={body} required onChange={(e) => setBody(e.target.value)}></textarea>
+            <textarea
+              id="body"
+              cols="30"
+              rows="10"
+              value={body}
+              required
+              onChange={(e) => setBody(e.target.value)}
+            ></textarea>
             <label htmlFor="author">Author:</label>
-            <input type="text" id="author" value ={author} onChange={(e) => setAuthor(e.target.value)}/>
-            <button>Add Blog</button>
-        </form>
-    </div>
-
-    </>;
+            <input
+              type="text"
+              id="author"
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+            />
+            {!loading && <button>Add Blog</button>}
+            {loading && <button disabled="disabled">Uploading...</button>}
+          </form>
+        </div>
+      )}
+    </>
+  );
 }
