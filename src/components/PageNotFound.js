0286b0e9013bc1a0c65e0b4a243a3c9cf46533f8
Manali Shetye <manalis826@gmail.com>
2022-01-23 23:15:40 +0530

    Modified file
    added inline styling

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -3,12 +3,30 @@ import { useHistory } from "react-router-dom";
 const PageNotFound = () => {
   const history = useHistory();
   return (
-    <div>
-      <h2>
+    <div
+      style={{
+        maxWidth: "400px",
+        margin: "0 auto",
+        textAlign: "center",
+      }}
+    >
+      <h2 style={{ color: "#f1356d", marginBottom: "25px" }}>
         404 Page Not Found
       </h2>
-      <p>Resource does not exist</p>
-      <button onClick={() => history.push("/")}>Go Back to Home</button>
+      <p style={{ marginBottom: "20px" }}>Resource does not exist</p>
+      <button
+        style={{
+          background: "#f1356d",
+          color: "#fff",
+          border: "0",
+          padding: "8px",
+          borderRadius: "8px",
+          cursor: "pointer",
+        }}
+        onClick={() => history.push("/")}
+      >
+        Go Back to Home Page
+      </button>
     </div>
   );
 };
