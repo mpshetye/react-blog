1f9979860b2055e60db872740e36f15c0d52d7c4
Manali Shetye <manalis826@gmail.com>
2022-01-23 19:48:07 +0530

    Modified file
    delete blog and redirect to home page

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,13 +1,31 @@
 import { useParams } from "react-router-dom";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "../customHooks/useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const url = `http://localhost:8000/blogs/${id}`;
+  const history = useHistory();
   const {
     data: blog,
     loading,
     error,
-  } = useFetch(`http://localhost:8000/blogs/${id}`);
+  } = useFetch(url);
+  const handleClick = async() =>{
+    try{
+      const res = fetch(url,{
+        method:'DELETE'
+      });
+      if (!res.ok) {
+        throw Error(`an error has occurred: ${res.status} ${res.statusText}`);
+      } else {
+        history.push('/');
+      }
+    } catch(err){
+      console.log(err.message);
+    }
+
+  }
   return (
     <div className="blog-details">
       {error && <div>{error}</div>}
@@ -17,6 +35,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>written by : {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick = {handleClick}>Delete</button>
         </article>
       }
     </div>
